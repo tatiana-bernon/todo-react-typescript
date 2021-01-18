0469cff9e3ac28035f9576356f329d61c90d023d
taty500summers <tatiana.bernon@gmail.com>
2021-01-18 16:00:21 +1300

    Add onChange

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 import { TodoListItem } from "./ToDoListItem";
 
 
-const todos: Array<Todo> = [
+const initialTodos: Array<Todo> = [
   {text: 'walk the dog', complete:true},
   {text: 'create app', complete:false}
 ]
 
 const App: React.FC = () => {
+  const [todos, setTodos] = useState(initialTodos);
+
+  const toggleTodo: ToggleTodo = selectedTodo => {
+    const newTodos = todos.map(todo => {
+      if (todo === selectedTodo) {
+        return {
+          ...todo,
+          completed: !todo.complete
+        }
+      }
+      return todo;
+    });
+    setTodos(newTodos);
+  }
+
   return (
     <>
-      <TodoListItem todo={todos[0]}/>
-      <TodoListItem todo={todos[1]}/>
+      <TodoListItem todo={todos[0]} toggleTodo={toggleTodo}/>
+      <TodoListItem todo={todos[1]} toggleTodo={toggleTodo}/>
     </>
   );
 };
--- src/ToDoListItem.tsx
+++ src/ToDoListItem.tsx
@@ -4,14 +4,18 @@ import "./TodoListItem.css"
 //Declares the type of TodoListItem using Interface (In TypeScript, interfaces fill the role of naming these types, and are a powerful way of defining contracts within your code as well as contracts with code outside of your project.)
 interface TodoListItemProps {
   todo: Todo;
+  toggleTodo: ToggleTodo;
 }
 
 //Generic type called FC allows to type our function components while taking into account Typescript Types
-export const TodoListItem: React.FC<TodoListItemProps> = ({ todo }) => {
+export const TodoListItem: React.FC<TodoListItemProps> = ({ todo, toggleTodo }) => {
   return (
     <li>
       <label className= {todo.complete? "complete" : "undefined"}>
-        <input type="checkbox" checked={todo.complete}/>
+        <input type="checkbox" 
+        checked={todo.complete}
+        onChange={() => toggleTodo(todo)}
+        />
         {todo.text}
       </label>
     </li>
